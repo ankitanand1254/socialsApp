72baf8aa28d2d90180635cc242d41cf3b6e3a2ab
Bzbob <ankitanand1254@gmail.com>
2022-10-21 16:59:14 +0530

    Second commit: Added routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 console.log('router loaded');
+
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+router.use('/users/posts', require('./users'));
 
 module.exports = router;
