ab18e54adcc59659a6c9c0287923b71ac12e06ef
Bzbob <ankitanand1254@gmail.com>
2022-11-07 11:17:31 +0530

    new commit

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,17 @@
-module.exports.posts = function(re,res){
+const Post = require('../models/post');
+
+module.exports.posts = function(req,res){
     return res.end('<h1>Posts</h1>');
+}
+module.exports.create = function(req,res){
+    Post.create({
+        content: req.body.content, 
+        user: req.user._id
+    }, function(err, post){
+        if(err){
+            console.log('error in creating post');
+            return;
+        }
+        return res.redirect('back');
+    });
 }
